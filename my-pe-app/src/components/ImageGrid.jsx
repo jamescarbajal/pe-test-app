0b5d20a1f19c65f7b8a45cd018cf54d394fd96d4
James C <you@example.com>
2025-07-13 14:46:54 -0700

    fix: image card sizing; position image card buttons

--- my-pe-app/src/components/ImageGrid.jsx
+++ my-pe-app/src/components/ImageGrid.jsx
@@ -1,27 +1,26 @@
 import { styled } from '@mui/material/styles';
 import { useState } from 'react';
-import { Link } from 'react-router';
-import HeroBox from '../components/HeroBox'
 import Box from '@mui/material/Box';
-import NavBar from '../components/NavBar'
-import Button from '@mui/material/Button'
-import UploadImageCard from '../components/UploadImageCard';
-import CropImageModal from '../components/cropImageModal';
+import UploadImageCard from './UploadImageCard';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 
 
 
 const Item = styled(Paper)(({ theme }) => ({
-    height:300,
+    height:275,
     width:250,
+    maxWidth:'fit-content',
+    display:'flex',
+    flexDirection:'column',
+    justifyContent:'space-between',
     backgroundColor: '#fff',
     ...theme.typography.body2,
     padding: theme.spacing(2),
     textAlign: 'center',
     color: (theme.vars ?? theme).palette.text.secondary,
     ...theme.applyStyles('dark', {
-    backgroundColor: '#1A2027',
+    backgroundColor: '#1A2027'
   }),
 }));
 
@@ -45,15 +44,16 @@ export default function ImageGrid(){
                 justifyContent:'center',
                 alignContent:'center',
                 alignItems:'center',
-                width:'fit-content'
+                width:'fit-content',
+                borderRadius:10
              }}>
             {Array.from(Array(9)).map((_, index) => (
             <Grid key={index} size={{ xs: 12, sm: 4, md: 4 }}
                 sx={{
                     display:'flex',
                     justifyContent:'center',
-                    alignItems:'center'
-                    
+                    alignItems:'center',
+                    height:'fit-content',
                 }}>
                 <Item>
                     <UploadImageCard/>
--- my-pe-app/src/components/UploadImageCard.jsx
+++ my-pe-app/src/components/UploadImageCard.jsx
@@ -9,41 +9,47 @@ import CardActions from '@mui/material/CardActions';
 import CropImageModal from './cropImageModal';
 import lizardImage from '../assets/images/iguanas-on-rocks-stockcake.jpg';
 
-export default function UploadImage() {
+export default function UploadImageCard() {
 
 
   return (
-    <Card variant="solid" sx={{ backgroundColor:'white', maxWidth: 345, maxHeight:'100%' }}>
+    <>
+    <Card variant="solid" sx={{ 
+        position:'relative',
+        backgroundColor:'white', 
+        width:250,
+        maxWidth: 345, 
+        display:'flex',
+        flexDirection:'column',
+        alignContent:'center',
+        justifyContent:'space-between',
+        height:'100%',
+      }}>
       <CardActionArea>
         <CardMedia
           component="img"
-          height="100px"
+          height="200px"
           image={lizardImage}
           alt="green iguana"
         />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
       </CardActionArea>
       <CardActions 
         sx={{
           display:'flex',
-          flexDirection:'row',
-          alignContent:'center',
-          justifyContent:'space-evenly',
-          width:'100%',
+          justifyContent:'space-between',
+          height:'fit-content',
         }}>
-        <Button size="small" variant="contained" color="black">
-          Share
+        <Button size="medium" variant="outlined" 
+        style={{
+          color:'black',
+          fontWeight:'bold',
+          border:'2px solid black'
+        }}>
+          Upload
         </Button>
         <CropImageModal/>
       </CardActions>
     </Card>
+    </>
   );
 }
