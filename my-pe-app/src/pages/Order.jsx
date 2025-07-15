09e18bee2e989c361324143ad70969516b1e7616
James C <you@example.com>
2025-07-14 20:30:46 -0700

    add: started component RadioButtons.jsx

--- my-pe-app/src/pages/Order.jsx
+++ my-pe-app/src/pages/Order.jsx
@@ -1,15 +1,9 @@
 import { styled } from '@mui/material/styles';
 import { useState } from 'react';
 import { Link } from 'react-router';
-import HeroBox from '../components/HeroBox'
-import Box from '@mui/material/Box';
-import NavBar from '../components/NavBar'
-import Button from '@mui/material/Button'
-import UploadImageCard from '../components/UploadImageCard';
-import CropImageModal from '../components/cropImageModal';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 import ImageGrid from '../components/ImageGrid';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
 
 
 export default function Order() {
@@ -19,7 +13,19 @@ export default function Order() {
 
     return (
     <>
-        <ImageGrid/>
+        <Container sx={{
+            width:'90vw',
+            height:'fit-content',
+            display:'flex',
+            flexDirection:'row'
+        }}>
+            <Box sx={{ flexGrow:1 }}>
+
+            </Box>
+            <Paper>
+                
+            </Paper>
+        </Container>
     </>
     )
 }
